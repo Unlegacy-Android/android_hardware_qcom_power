e27f6185f68540d21190d09f6f06210207a86207
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-09 22:21:25 +0100

    Convert to binderized HAL 1.0
    Mostly based on the wahoo power HAL.
    
    Change-Id: Ibd47833e77475df155c7f2d88f37f9ec04817c13
    Signed-off-by: Dániel Járai <jaraidaniel@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,20 +1,28 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-  ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
-    LOCAL_PATH := $(call my-dir)
+ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
 
-    # HAL module implemenation stored in
-    # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
-    include $(CLEAR_VARS)
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
 
-    LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-    LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-    LOCAL_SRC_FILES := \
-                       power.c \
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := qcom
+LOCAL_MODULE_TAGS := optional
 
-    LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
-    LOCAL_MODULE_TAGS := optional
-    include $(BUILD_SHARED_LIBRARY)
-  endif
+LOCAL_MODULE := android.hardware.power@1.0-service.qcom
+LOCAL_INIT_RC := android.hardware.power@1.0-service.qcom.rc
+LOCAL_SRC_FILES := service.cpp \
+                   Power.cpp \
+                   power-helper.c
+
+LOCAL_SHARED_LIBRARIES := liblog \
+                          libcutils \
+                          libhidlbase \
+                          libhidltransport \
+                          libhardware \
+                          libutils \
+                          android.hardware.power@1.0
+
+include $(BUILD_EXECUTABLE)
+
 endif
